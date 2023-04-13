0865583c2cc69c9f9eab2030406302444fe33bc3
Atharv <atharvtelkar@gmail.com>
2023-04-13 19:56:57 +0530

    1.5

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from flask import Flask, request
 
 # Create your views here.
 def index(request):
+    print(request.POST)
     return render(request, "index.html")
+
+def result(request):
+    if request.method=='POST':
+        age=request.POST["number"]
+        gender=request.POST["gender"]
+        symptoms=request.POST["symptoms"]
+        obj = {
+            'age':age,
+            'gender':gender,
+            'symptoms':symptoms,
+        }  
+
+        symptoms_array = list(filter(lambda symptom: symptom != '', obj['symptoms'].split(',\r\n')))
+        new_object = {
+            'age': obj['age'],
+            'gender': obj['gender'],
+            'symptoms': symptoms_array
+        }
+        print(new_object)
+        return render(request,"result.html")
+
+
+
+
