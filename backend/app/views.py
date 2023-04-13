d160afe1b00b1cdb22411771eff34cc956d3604c
Het Pandya <202151065@iiitvadodara.ac.in>
2023-04-13 16:09:38 +0530

    2.3

--- backend/app/views.py
+++ backend/app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "symptom/index.html")
+    return render(request, "index.html")
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
 from django.urls import path
+from django.conf.urls import include
 
-urlpatterns = patterns('',url(r'^admin', include(admin.site.urls)),
-                       url(r'^home/','app.views.index', name='Home'),
-)
-
+urlpatterns = [
+    path('admin/',admin.site.urls),
+    path('',include('app.urls'))
+    ]
