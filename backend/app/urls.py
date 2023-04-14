13c52a39ea5bca34bbe3df370e3a9e63bbedaa5e
Atharv <atharvtelkar@gmail.com>
2023-04-14 11:02:37 +0530

    1.8

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import index,result
+from .views import index,result,restart_server
 
 urlpatterns = [
     path('', index, name='Home'),
     path('result/',result,name='Results'),
-   
+    path('',restart_server,name="Re"),
 ]
